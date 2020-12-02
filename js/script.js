b5a6e2c8489a6d626cfced74cc7c0c55debcfbfa
Manten Devillé <manten.d@hotmail.com>
2020-12-02 11:28:17 +0100

    Add variables for days & click function

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const weatherDayOne = document.querySelector('.weatherDayOne');
+const weatherDayTwo = document.querySelector('.weatherDayTwo');
+const weatherDayThree = document.querySelector('.weatherDayThree');
+const weatherDayFour = document.querySelector('.weatherDayFour');
+const weatherDayFive = document.querySelector('.weatherDayFive');
+
+const city = document.querySelector('.city');
+
+
+document.querySelector('#run').addEventListener('click', () => {
+
+
+
+
+});
