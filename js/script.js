1bdea834ae1ae502770cf19b60edd0019fa048bc
Manten Devillé <manten.d@hotmail.com>
2020-12-03 10:26:20 +0100

    Add fetch API & alert if empty form

--- js/script.js
+++ js/script.js
@@ -9,8 +9,22 @@ const weatherDayFive = document.querySelector('.weatherDayFive');
 const city = document.querySelector('.city');
 
 
+const api = {
+    key: "e9bbcf9d2a14bd33b5c8c0b51d3cb1eb",
+    base: "http://api.openweathermap.org/data/2.5/",
+}
+
 button.addEventListener('click', () => {
 
+    console.log(city.value);
+
+    if (city.value.length == 0) {
+        alert('Please enter a location!')
+    };
+
+    fetch(`${api.base}weather?q=${city.value}&units=metric&appid=${api.key}`)
+        .then(weather => weather.json())
+        .then(data => console.log(data));
 
 
 
