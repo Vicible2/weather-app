92547983d613fc504724d2a0ba541b363ae91043
Manten Devillé <manten.d@hotmail.com>
2020-12-07 10:23:24 +0100

    Added functions to select data and add to HTML
    Added functions to access data from first fetch:
    -Function getDailyTempData to get coord data,
    -Call getdailyTempdata to use coords for new fetch
    -get daily forecast data from getDailyTempData function
    -use second fetch inside function
    -use tempSelect to insert chosen data into innerHTML

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ const weatherDayFive = document.querySelector('.weatherDayFive');
 
 const city = document.querySelector('.city');
 
-
+//API variables
 const api = {
     key: "e9bbcf9d2a14bd33b5c8c0b51d3cb1eb",
     base: "http://api.openweathermap.org/data/2.5/",
@@ -19,38 +19,72 @@ const apiImg = {
     base: "https://api.unsplash.com/",
 }
 
+//coordinates variables
+
+let coords = [];
+let dailyTemps = [];
+
 button.addEventListener('click', () => {
 
     console.log(city.value);
-
+    //check if form is empty, error if true
     if (city.value.length == 0) {
         alert('Please enter a location!')
     };
 
+    //Fetch the weather of today to get Coordinates
     fetch(`${api.base}weather?q=${city.value}&units=metric&appid=${api.key}`)
         .then(weather => {
             return weather.json()
         })
         .then(data => {
             weatherDayOne.innerHTML = data.main.temp;
+            coords = data.coord;
+            getDailyTempData();
+            return console.log(data);
 
-            return console.log(data)
         })
-        
 
-        /*
 
+
+    //function to get daily forecast
+    
+    function getDailyTempData() {
+        console.log(coords.lon, coords.lat);
+        fetch(`${api.base}onecall?lat=${coords.lat}&lon=${coords.lon}&exclude=minutely,hourly,alerts&units=metric&appid=${api.key}`)
+            .then(dailyWeather => {
+                return dailyWeather.json();
+            })
+            .then(dailyData => {
+                dailyTemps = dailyData.daily;
+                console.log(dailyTemps);
+                tempSelect();
+                return console.log(dailyData);
+            })
+    }
+    function tempSelect() {
+       weatherDayOne.innerHTML = dailyTemps[1]["temp"]["day"];
+       weatherDayTwo.innerHTML = dailyTemps[2]["temp"]["day"];
+       weatherDayThree.innerHTML = dailyTemps[3]["temp"]["day"];
+       weatherDayFour.innerHTML = dailyTemps[4]["temp"]["day"];
+       weatherDayFive.innerHTML = dailyTemps[5]["temp"]["day"];
+
+    }
+    /*
+    
         regular weather call -> take lat/long coordinates. Insert as variables in https://openweathermap.org/api/one-call-api .
         add &units=metric for Celcius -> select value of daily.temp.day (avg temp during the day)
 
-        */
+    */
 
+
+    //Get random background for every city filled in form
     fetch(`${apiImg.base}search/photos/?query=${city.value}&client_id=${apiImg.key}`)
         .then(background => {
             return background.json()
         })
         .then(data => {
-            let randomSelector = Math.floor(Math.random () * 5);
+            let randomSelector = Math.floor(Math.random() * 5);
             let imageUrl = data["results"][randomSelector]["urls"]["full"]
             document.body.style.backgroundImage = `url(${imageUrl})`;
             document.body.style.backgroundSize = "cover";
